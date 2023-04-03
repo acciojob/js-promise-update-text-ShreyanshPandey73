bb3b4e3f1b4a84e59881e9cde27c9354ebfdbc81
ShreyanshPandey73 <117717418+ShreyanshPandey73@users.noreply.github.com>
2023-04-03 22:43:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+//your JS code here. If required.
+function sayHello(){
+	return new Promise((resolve) =>{
+		setTimeout(() => {
+			resolve("Hello, world!");
+		}, 1000);
+	});
+}
+const output = document.getElementById("output");
+sayHello().then((result) =>
+	{
+		output.textContent = result;
+	});
